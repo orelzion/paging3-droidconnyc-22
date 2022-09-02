2319059fd12d85fc140b63648d5c4f9f6fba4aeb
orelzion <orel@viz.ai>
2022-09-02 09:25:10 -0400

    better bookmark handling of patient photo

--- app/src/main/java/com/example/droidconnyc22/view/PatientViewHolder.kt
+++ app/src/main/java/com/example/droidconnyc22/view/PatientViewHolder.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
+import com.example.droidconnyc22.BuildConfig
 import com.example.droidconnyc22.R
 import com.example.droidconnyc22.databinding.PatientViewHolderBinding
 import com.example.droidconnyc22.model.Patient
@@ -26,7 +27,7 @@ class PatientViewHolder(private val viewBinding: PatientViewHolderBinding) :
 
     fun bind(patient: Patient, onBookmarkCheckChanged: (checked: Boolean) -> Unit) {
         viewBinding.patientName.text = patient.name
-        viewBinding.scanImage.load(patient.photoUrl) {
+        viewBinding.scanImage.load("${BuildConfig.LOCAL_IP}${patient.photoUrl}") {
             crossfade(true)
             fallback(R.drawable.no_brain)
         }
